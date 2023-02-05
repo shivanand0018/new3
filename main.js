859d00620e8650784e76b1d3cda9ec24c891a854
shivanand0018@gmail.com <shivanand0018@gmail.com>
2023-02-05 13:11:40 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,40 @@
+var form=document.getElementById('addForm')
+var item=document.getElementById('items')
+
+form.addEventListener('submit',addItem);
+item.addEventListener('click',removeItem)
+
+function addItem(e)
+{
+    e.preventDefault();
+
+    var newItem=document.getElementById('item').value;
+    console.log(newItem)
+    var li=document.createElement('li');
+    li.className='list-group-item';
+    li.appendChild(document.createTextNode(newItem));
+    item.appendChild(li);
+
+    var deleteBtn = document.createElement('button');
+    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+    deleteBtn.appendChild(document.createTextNode('X'));
+    
+    var editBtn=document.createElement('edit');
+    editBtn.className='btn btn-info edit';
+    editBtn.appendChild(document.createTextNode('edit'))
+    li.appendChild(editBtn);
+    li.appendChild(deleteBtn)
+    item.appendChild(li);
+}
+
+function removeItem(e)
+{
+    console.log(e.target.parentElement)
+    if(e.target.classList.contains('delete'))
+    {
+        if(confirm('Are You Sure?')){
+            var li = e.target.parentElement;
+            item.removeChild(li);
+         }
+    }
+}
